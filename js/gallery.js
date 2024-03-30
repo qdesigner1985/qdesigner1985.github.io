17dd155ad63661ff276f34ddf12eed12ebba34b0
Jakub Krzeminski <qdesigner1985@gmail.com>
2024-03-30 15:23:40 +0100

    Initial commit

---
+++ js/gallery.js
@@ -0,0 +1,142 @@
+class Slideshow {
+    constructor(el) {
+        
+        this.DOM = {el: el};
+      
+        this.config = {
+          slideshow: {
+            delay: 3000,
+            pagination: {
+              duration: 3,
+            }
+          }
+        };
+        this.init();
+      
+    }
+    init() {
+      
+      var self = this;
+      this.DOM.slideTitle = this.DOM.el.querySelectorAll('.slide-title');
+      this.DOM.slideTitle.forEach((slideTitle) => {
+        charming(slideTitle);
+      });
+      this.slideshow = new Swiper (this.DOM.el, {
+          
+          loop: true,
+          autoplay: {
+            delay: this.config.slideshow.delay,
+            disableOnInteraction: false,
+          },
+          speed: 500,
+          preloadImages: true,
+          updateOnImagesReady: true,
+        
+          pagination: {
+            el: '.slideshow-pagination',
+            clickable: true,
+            bulletClass: 'slideshow-pagination-item',
+            bulletActiveClass: 'active',
+            clickableClass: 'slideshow-pagination-clickable',
+            modifierClass: 'slideshow-pagination-',
+            renderBullet: function (index, className) {
+              
+              var slideIndex = index,
+                  number = (index <= 8) ? '0' + (slideIndex + 1) : (slideIndex + 1);
+              
+              var paginationItem = '<span class="slideshow-pagination-item">';
+              paginationItem += '<span class="pagination-number">' + number + '</span>';
+              paginationItem = (index <= 8) ? paginationItem + '<span class="pagination-separator"><span class="pagination-separator-loader"></span></span>' : paginationItem;
+              paginationItem += '</span>';
+            
+              return paginationItem;
+              
+            },
+          },
+          navigation: {
+            nextEl: '.slideshow-navigation-button.next',
+            prevEl: '.slideshow-navigation-button.prev',
+          },
+          scrollbar: {
+            el: '.swiper-scrollbar',
+          },
+        
+          on: {
+            init: function() {
+              self.animate('next');
+            },
+          }
+        
+        });
+        this.initEvents();
+        
+    }
+    initEvents() {
+        
+        this.slideshow.on('paginationUpdate', (swiper, paginationEl) => this.animatePagination(swiper, paginationEl));
+
+        this.slideshow.on('slideNextTransitionStart', () => this.animate('next'));
+        
+        this.slideshow.on('slidePrevTransitionStart', () => this.animate('prev'));
+            
+    }
+    animate(direction = 'next') {
+     
+        this.DOM.activeSlide = this.DOM.el.querySelector('.swiper-slide-active'),
+        this.DOM.activeSlideImg = this.DOM.activeSlide.querySelector('.slide-image'),
+        this.DOM.activeSlideTitle = this.DOM.activeSlide.querySelector('.slide-title'),
+        this.DOM.activeSlideTitleLetters = this.DOM.activeSlideTitle.querySelectorAll('span');
+
+        this.DOM.activeSlideTitleLetters = direction === "next" ? this.DOM.activeSlideTitleLetters : [].slice.call(this.DOM.activeSlideTitleLetters).reverse();
+
+        this.DOM.oldSlide = direction === "next" ? this.DOM.el.querySelector('.swiper-slide-prev') : this.DOM.el.querySelector('.swiper-slide-next');
+        if (this.DOM.oldSlide) {
+          this.DOM.oldSlideTitle = this.DOM.oldSlide.querySelector('.slide-title'),
+          this.DOM.oldSlideTitleLetters = this.DOM.oldSlideTitle.querySelectorAll('span'); 
+          this.DOM.oldSlideTitleLetters.forEach((letter,pos) => {
+            TweenMax.to(letter, .3, {
+              ease: Quart.easeIn,
+              delay: (this.DOM.oldSlideTitleLetters.length-pos-1)*.04,
+              y: '50%',
+              opacity: 0
+            });
+          });
+        }
+
+        this.DOM.activeSlideTitleLetters.forEach((letter,pos) => {
+					TweenMax.to(letter, .6, {
+						ease: Back.easeOut,
+						delay: pos*.05,
+						startAt: {y: '50%', opacity: 0},
+						y: '0%',
+						opacity: 1
+					});
+				});
+
+        TweenMax.to(this.DOM.activeSlideImg, 1.5, {
+            ease: Expo.easeOut,
+            startAt: {x: direction === 'next' ? 200 : -200},
+            x: 0,
+        });
+
+    
+    }
+    animatePagination(swiper, paginationEl) {
+
+      this.DOM.paginationItemsLoader = paginationEl.querySelectorAll('.pagination-separator-loader');
+      this.DOM.activePaginationItem = paginationEl.querySelector('.slideshow-pagination-item.active');
+      this.DOM.activePaginationItemLoader = this.DOM.activePaginationItem.querySelector('.pagination-separator-loader');
+      
+      console.log(swiper.pagination);
+        TweenMax.set(this.DOM.paginationItemsLoader, {scaleX: 0});
+        TweenMax.to(this.DOM.activePaginationItemLoader, this.config.slideshow.pagination.duration, {
+          startAt: {scaleX: 0},
+          scaleX: 1,
+        });
+      
+      
+    }
+    
+}
+
+const slideshow = new Slideshow(document.querySelector('.slideshow'));
